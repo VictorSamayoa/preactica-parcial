598501427ce9fe0ebe682e9b471c5c1655cc3faf
Victor Josue Samayoa Ortiz <josuesamayoa064@gmail.com>
2024-04-17 07:21:42 -0600

    agregando clase1

---
+++ proyecto/include/clase_submenu1.h
@@ -0,0 +1,16 @@
+#ifndef CLASE_SUBMENU1_H
+#define CLASE_SUBMENU1_H
+
+
+class clase_submenu1
+{
+    public:
+        clase_submenu1();
+        virtual ~clase_submenu1();
+
+    protected:
+
+    private:
+};
+
+#endif // CLASE_SUBMENU1_H
---
+++ proyecto/src/clase_submenu1.cpp
@@ -0,0 +1,11 @@
+#include "clase_submenu1.h"
+
+clase_submenu1::clase_submenu1()
+{
+    //ctor
+}
+
+clase_submenu1::~clase_submenu1()
+{
+    //dtor
+}
