6097502a481859d337ca1faf6eb3358bb3c3f183
Victor Josue Samayoa Ortiz <josuesamayoa064@gmail.com>
2024-04-17 07:33:23 -0600

    subida submenu 2

---
+++ proyecto/include/clases_submenu3.h
@@ -0,0 +1,15 @@
+#ifndef CLASES_SUBMENU3_H
+#define CLASES_SUBMENU3_H
+
+
+class clases_submenu3
+{
+    public:
+        clases_submenu3();
+
+    protected:
+
+    private:
+};
+
+#endif // CLASES_SUBMENU3_H
---
+++ proyecto/src/clases_submenu3.cpp
@@ -0,0 +1,6 @@
+#include "clases_submenu3.h"
+
+clases_submenu3::clases_submenu3()
+{
+    //ctor
+}
